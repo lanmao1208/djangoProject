8a0bc6e2a6d79541d6565f1af735b9538a2f41fa
张 <1692230407@qq.com>
2023-02-17 17:15:36 +0800

    第一次运行通过,更新了urls路由文件和projects的方法

--- projects/serializer.py
+++ projects/serializer.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
-from .models import ProjectsModels
+from projects.models import ProjectsModels
 from rest_framework import validators
 
-class ProjectsSerializer(serializers.Serializer):
+# class ProjectsSerializer(serializers.Serializer):
+#     names =serializers.CharField(max_length=60, min_length=2, label='项目名称', help_text='项目名称',
+#                                  validators=[validators.UniqueValidator(queryset=ProjectsModels.objects.all(),message='该项目已存在')])
+#     leader = serializers.CharField(max_length=100, label='项目负责人', help_text='项目负责人')
+#     leader_phone = serializers.CharField(max_length=60, label='项目负责人电话', required=False)
+#     testers = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员', required=False)
+#     programmer = serializers.CharField(max_length=200, label='开发人员', help_text='开发人员', required=False)
+#     project_states = serializers.CharField(max_length=60, label='项目状态', help_text='结束:0;进行中:1;暂停:2')
+
+
+class ProjectsSerializer(serializers.ModelSerializer):
     names =serializers.CharField(max_length=60, min_length=2, label='项目名称', help_text='项目名称',
                                  validators=[validators.UniqueValidator(queryset=ProjectsModels.objects.all(),message='该项目已存在')])
-    leader = serializers.CharField(max_length=100, label='项目负责人', help_text='项目负责人',
-                                   validators=[validators.UniqueForYearValidator])
-    leader_phone = serializers.CharField(max_length=60, label='项目负责人电话', required=False)
-    testers = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员', required=False)
-    programmer = serializers.CharField(max_length=200, label='开发人员', help_text='开发人员', required=False)
-    project_states = serializers.CharField(max_length=60, label='项目状态', help_text='结束:0;进行中:1;暂停:2')
+    class Meta:
+        model = ProjectsModels
+        fields = '__all__'
 
 
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from .models import ProjectsModels
+from .serializer import ProjectsSerializer
+from django.http import JsonResponse
 # Create your views here.
 
 class ProjectsView(View):
-    pass
+    def get(self, request):
+        qs = ProjectsModels.objects.all()
+        pro_obj = ProjectsSerializer(instance=qs, many=True)
+        return JsonResponse(pro_obj.data, status=200, safe=False)
+
+    def post(self, request):
+        pass
+
+class ProjectsDetailView(View):
+    def get(self, request, pk):
+        qs = ProjectsModels.objects.get(pk)
+        pro_obj = ProjectsSerializer(instance=qs)
+        return JsonResponse(pro_obj.data, status=200)
